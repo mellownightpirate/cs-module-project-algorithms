c0e7c8b8b2183fa8a667ecae0462bec051b689ea
mellownightpirate <a@hasan.co>
2020-06-17 16:45:04 +0100

    sliding window max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,8 +4,13 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+        win = []
+        small = [0] * k
+        for i in range(0, len(nums) - k + 1):
+                small = nums[i:i + k]
+                win.append(max(small))
 
-    pass
+        return win
 
 
 if __name__ == '__main__':
